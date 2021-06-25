c9443df59e21cf90df2e3a9101bf5d7b765c6b35
Avery.Davenport <averymdavenport@gmail.com>
2021-06-25 17:39:53 -0600

    5

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -1,5 +1,4 @@
 import { Builder, Capabilities, By } from "selenium-webdriver"
-// const { counterMove } = require('./server');
 
 const chromedriver = require('chromedriver')
 
@@ -21,25 +20,28 @@ describe('Game Testing', function(){
         await button.click();
     });
 
-    test('I can select boxs', async () => {
-        let square = await (await driver).findElement(By.id('cell-7'));
-        await square.click()
+    test('Can I add an X', async () => {
+        let square7 = await driver.findElement(By.id('cell-7'));
+        await square7.click()
+        expect(await square7.getText()).toEqual('X')
     });
 
-    // test('Check if O selects a box', async ()=>{
-    //     expect(counterMove).toBeTruthy()
-    //     // let square = await (await driver).findElement(By.id('cell-0'));
-    //     // console.log(square)
-    // });
+    test('Check if O selects a box', async ()=>{
+        let square0 = await (await driver).findElement(By.id('cell-0'));
+        expect(await square0.getText()).toEqual('O');
+    });
 
-    // test('')
+    test('Adding another X, with Xpath', async () => {
+        driver.navigate().refresh();
+        await driver.sleep(1000);
+        let button = await (await driver).findElement(By.id('start-game'));
+        await button.click();
+        await driver.sleep(1000);
 
-    test('I can select boxs', async () => {
-        let square1 = await (await driver).findElement(By.id('cell-6'));
-        let square2 = await (await driver).findElement(By.id('cell-2'));
-        await square1.click()
-        await square2.click()
-        await driver.sleep(5000)
+        let square6 = await driver.findElement(By.xpath("//td[@id='cell-6']"));
+        await square6.click()
+        expect(await square6.getText()).toEqual('X')
+        await driver.sleep(2000)
     });
 })
 
